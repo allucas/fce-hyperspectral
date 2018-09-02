8a6aaf1859cde5fdff2f9240422e678bec19a6f5
CabralesPC <alfredo1238>
2018-09-01 20:17:02 -0700

    Fixed speckle filter code

--- speckle_filter.py
+++ speckle_filter.py
@@ -113,16 +113,18 @@ def GoDec(X,rank,card,power, error_bound):
 
 
 
-os.chdir('E:\\Research Data\\hyperspectral\\dilution\\H006\\filtered')
+os.chdir('F:\\Research Data\\hyperspectral\\dilution\\H006\\filtered')
 lib, img = load_hyper_image('E3_2_c.bil')
 lamb = np.array(lib['wavelength']).astype(float)
 img_all = img.load()
-
+img_all = img_all[0:250,0:500,:]
 ## Apply the GoDec algorithm to the image
 # Initialize the first patch
 step=5
-img_filt = np.zeros(img_all2.shape)
-patch = img_all2[0:q,0:q,:]
+q = 25
+img_filt = np.zeros(img_all.shape)
+patch = img_all[0:q,0:q,:]
+patch_mat = np.zeros([q*q,img_all.shape[2]])
 for k in range(patch.shape[2]):
     patch_mat[:,k] = np.reshape(patch[:,:,k],q*q)
 L,S = GoDec(patch_mat,7,4000,10,error_bound=10)
@@ -131,9 +133,9 @@ img_filt[0:q,0:q,:] = L_mat
 patch_old = L_mat
 
 # Initialize first column
-for i in range(step,img_all2.shape[0]-q,step):
+for i in range(step,img_all.shape[0]-q,step):
     j=0
-    patch = img_all2[0+i:q+i,0+j:q+j,:]
+    patch = img_all[0+i:q+i,0+j:q+j,:]
     for k in range(patch.shape[2]):
         patch_mat[:,k] = np.reshape(patch[:,:,k],q*q)
     L,S = GoDec(patch_mat,7,4000,10,error_bound=10)
@@ -147,9 +149,9 @@ for i in range(step,img_all2.shape[0]-q,step):
 # Initialize first row
 patch_old = img_filt[0+i:q+i,0+j:q+j,:]
 
-for j in range(step,img_all2.shape[1]-q,step):
+for j in range(step,img_all.shape[1]-q,step):
     i=0
-    patch = img_all2[0+i:q+i,0+j:q+j,:]
+    patch = img_all[0+i:q+i,0+j:q+j,:]
     for k in range(patch.shape[2]):
         patch_mat[:,k] = np.reshape(patch[:,:,k],q*q)
     L,S = GoDec(patch_mat,7,4000,10,error_bound=10)
@@ -161,13 +163,13 @@ for j in range(step,img_all2.shape[1]-q,step):
     patch_old = patch_new
 
 # Run the algorithm in the remaining patches
-for i in range(step,img_all2.shape[0]-q,step):
-    for j in range(step*2,img_all2.shape[1]-q,step):
-        patch = img_all2[0+i:q+i,0+j:q+j,:]
+for i in range(step,img_all.shape[0]-q,step):
+    for j in range(step*2,img_all.shape[1]-q,step):
+        patch = img_all[0+i:q+i,0+j:q+j,:]
         for k in range(patch.shape[2]):
             patch_mat[:,k] = np.reshape(patch[:,:,k],q*q)
         L,S = GoDec(patch_mat,7,4000,10,error_bound=10)
-        print(j)
+        print('Column: ',j, '. Row: ', i)
         L_mat = np.reshape(L,[q,q,300])
         patch_new = L_mat.copy()
         patch_new[0:-step,0:-step] = (patch_new[0:-step,0:-step]+patch_old[step:,step:])/2
